69a643361c0d7d8a85a09b8cb26fe30b7ffefbea
Idrees Muhammad Qazi <qamikazi@outlook.com>
2025-10-22 11:47:08 +0000

    Auto-commit: Agent tool execution

--- src/components/Auth/AuthModal.tsx
+++ src/components/Auth/AuthModal.tsx
@@ -130,7 +130,7 @@ export default function AuthModal({ onClose }: AuthModalProps) {
         </button>
 
         <button
-          onClick={handleTryNow}
+          onClick={onClose}
           className={styles.tryNowButton}
           type="button"
         >
